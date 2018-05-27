b86e9ff03a950ab4812d0f9f551352c183417fd8
thegeekkid <brian@bscc.support>
2018-05-27 17:45:57 -0500

    Added production mode flip and P.Mode GUI persistence

--- ProductionReady/Form1.Designer.cs
+++ ProductionReady/Form1.Designer.cs
@@ -30,16 +30,16 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.checkBox6 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
@@ -57,6 +57,16 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Select production days";
             this.groupBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save production days";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // checkedListBox1
             // 
@@ -90,56 +100,15 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Select production settings";
             // 
-            // checkBox1
+            // button1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(7, 20);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(151, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Disable Windows Updates";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(7, 43);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(176, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Change wallpaper to solid black";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(7, 66);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(138, 17);
-            this.checkBox3.TabIndex = 2;
-            this.checkBox3.Text = "Disable automatic sleep";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox4
-            // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(7, 89);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(135, 17);
-            this.checkBox4.TabIndex = 3;
-            this.checkBox4.Text = "Disable monitor timeout";
-            this.checkBox4.UseVisualStyleBackColor = true;
-            // 
-            // checkBox5
-            // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(7, 112);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(122, 17);
-            this.checkBox5.TabIndex = 4;
-            this.checkBox5.Text = "Disable screensaver";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(7, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save settings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox6
             // 
@@ -152,25 +121,56 @@
             this.checkBox6.UseVisualStyleBackColor = true;
             this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
             // 
-            // button1
+            // checkBox5
             // 
-            this.button1.Location = new System.Drawing.Point(7, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(198, 33);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Save settings";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(7, 112);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(122, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Disable screensaver";
+            this.checkBox5.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // checkBox4
             // 
-            this.button2.Location = new System.Drawing.Point(6, 158);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(213, 33);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Save production days";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(7, 89);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(135, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Disable monitor timeout";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 66);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(138, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Disable automatic sleep";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 43);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(176, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Change wallpaper to solid black";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(151, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Disable Windows Updates";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // button3
             // 
@@ -181,6 +181,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Enable production mode";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -191,6 +192,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "Disable production mode";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
@@ -206,6 +208,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Production Ready Settings";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
--- ProductionReady/Form1.cs
+++ ProductionReady/Form1.cs
@@ -18,6 +18,8 @@ namespace ProductionReady
             InitializeComponent();
         }
 
+        
+
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if (checkBox1.Checked)
@@ -44,6 +46,11 @@ namespace ProductionReady
                     groupBox1.Visible = false;
                 }
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveSettings();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -76,7 +83,6 @@ namespace ProductionReady
                 root.SetValue("DisableTimeout", checkBox4.Checked.ToString());
                 root.SetValue("DisableScreensaver", checkBox5.Checked.ToString());
                 root.SetValue("PreventShutdown", checkBox6.Checked.ToString());
-                root.Close();
 
                 // Alert user
                 MessageBox.Show("Settings saved!");
@@ -124,9 +130,112 @@ namespace ProductionReady
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void readGUI()
         {
-            saveSettings();
+            // Check if registry keys exist yet or not
+            if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Semrau Software Consulting\ProductionReady", "DisableUpdates", null) != null)
+            {
+
+                //MessageBox.Show("Entering!");
+                RegistryKey root = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting").OpenSubKey("ProductionReady");
+
+                checkBox1.Checked = bool.Parse(root.GetValue("DisableUpdates").ToString());
+                checkBox2.Checked = bool.Parse(root.GetValue("ChangeWallpaper").ToString());
+                checkBox3.Checked = bool.Parse(root.GetValue("DisableSleep").ToString());
+                checkBox4.Checked = bool.Parse(root.GetValue("DisableTimeout").ToString());
+                checkBox5.Checked = bool.Parse(root.GetValue("DisableScreensaver").ToString());
+                checkBox6.Checked = bool.Parse(root.GetValue("PreventShutdown").ToString());
+
+            }
+
+            // Read production days
+            if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Semrau Software Consulting\ProductionReady", "ProductionDays", null) != null)
+            {
+                RegistryKey root = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting").OpenSubKey("ProductionReady");
+
+                string daysarray = root.GetValue("ProductionDays").ToString();
+                
+                foreach(string i in daysarray.Split(','))
+                {
+                    checkedListBox1.SetItemChecked(int.Parse(i), true);
+                }
+            }
+
+
+            // Check if production mode is enabled yet
+            if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Semrau Software Consulting\ProductionReady", "pmode", null) != null)
+            {
+                Registry.LocalMachine.OpenSubKey("SOFTWARE", true).CreateSubKey("Semrau Software Consulting");
+                Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting", true).CreateSubKey("ProductionReady");
+
+                RegistryKey root = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting").OpenSubKey("ProductionReady");
+
+                if (root.GetValue("pmode").ToString() == "True")
+                {
+                    EnablePmodeGUI();
+                }else
+                {
+                    DisablePmodeGUI();
+                }
+            }else
+            {
+                // Production mode is disabled if this isn't set.
+                DisablePmodeGUI();
+            }
+
+            
+
+        }
+
+        private void EnablePmode() {
+            // Check if registry keys exist yet or not
+            if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Semrau Software Consulting\ProductionReady", "DisableUpdates", null) == null)
+            {
+                Registry.LocalMachine.OpenSubKey("SOFTWARE", true).CreateSubKey("Semrau Software Consulting");
+                Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting", true).CreateSubKey("ProductionReady");
+            }
+
+            RegistryKey root = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting").OpenSubKey("ProductionReady", true);
+
+            root.SetValue("pmode", "True");
+
+            EnablePmodeGUI();
+        }
+
+
+        private void EnablePmodeGUI()
+        {
+            this.button3.Enabled = false;
+            this.button4.Enabled = true;
+        }
+
+        private void DisablePmode()
+        {
+            RegistryKey root = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting").OpenSubKey("ProductionReady", true);
+            root.SetValue("pmode", "False");
+
+            DisablePmodeGUI();
+        }
+
+        private void DisablePmodeGUI()
+        {
+            this.button3.Enabled = true;
+            this.button4.Enabled = false;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            readGUI();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EnablePmode();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DisablePmode();
         }
     }
 }
