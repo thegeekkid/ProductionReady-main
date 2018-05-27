e0ef8e57aa4c175b6a1d8fa618f123564010a63b
thegeekkid <brian@bscc.support>
2018-05-27 17:16:24 -0500

    Added ability to save settings

--- ProductionReady/Form1.cs
+++ ProductionReady/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace ProductionReady
 {
@@ -51,6 +52,39 @@ namespace ProductionReady
             {
                 MessageBox.Show("Error: you can not select more than 3 production days.  Updates and reboots are an important part of keeping your computer safe and running smoothly.");
             }
+        }
+
+        private void saveSettings()
+        {
+            try
+            {
+                // Check if registry keys exist yet or not
+                if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Semrau Software Consulting\ProductionReady", "DisableUpdates", null) == null)
+                {
+                    Registry.LocalMachine.OpenSubKey("SOFTWARE", true).CreateSubKey("Semrau Software Consulting");
+                    Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting", true).CreateSubKey("ProductionReady");
+                    RegistryKey root = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Semrau Software Consulting").OpenSubKey("ProductionReady", true);
+                    root.SetValue("DisableUpdates", checkBox1.Checked.ToString());
+                    root.SetValue("ChangeWallpaper", checkBox2.Checked.ToString());
+                    root.SetValue("DisableSleep", checkBox3.Checked.ToString());
+                    root.SetValue("DisableTimeout", checkBox4.Checked.ToString());
+                    root.SetValue("DisableScreensaver", checkBox5.Checked.ToString());
+                    root.SetValue("PreventShutdown", checkBox6.Checked.ToString());
+                }
+
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Error saving settings:" + Environment.NewLine + ex.ToString());
+            }
+        }
+        private void saveDays()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveSettings();
         }
     }
 }
